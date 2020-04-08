29cb1d2bc46b277f55358e4653a16cc9176f023e
Duke Ko <dukhyunko93@gmail.com>
2020-04-08 21:11:19 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -60,7 +60,7 @@ class TicTacToe
   end
 
   def won?
-    WIN_COMBINATIONS.detect{|combo| @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X" || @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O"}
+    combonations = WIN_COMBINATIONS.detect{|combo| @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X" || @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O"}
     return combo
   end
 
